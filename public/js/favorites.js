c1c8cbc6e168eea5f2f86ef8b3c63a4bedd46e5c
Emily <emily@skrivapa.se>
2013-03-02 18:14:42 +0100

    make a start on jscript

---
+++ public/js/favorites.js
@@ -0,0 +1,3 @@
+window.Favorites = Backbone.Model.extend({
+  urlRoot: "/openapi.etsy.com/v2/users/82emily/favorites/listings?api_key=3sz9ulqq0kp94uyubmoktn5w"
+});
--- server.js
+++ server.js
@@ -7,7 +7,27 @@ var express = require('express'),
 var app = connect()
   .use(connect.logger('dev'))
   .use(connect.static('public'))
+  .use(function(req, res, next) {
+    if (req.url.indexOf('/openapi.etsy.com')===0) {
+      var options = {
+        host: 'openapi.etsy.com',
+        path: req.url.substring(17),
+        method: 'GET'
+      };
 
-var port = process.env.PORT || 8080;
+      callback = function(response) {
+        response.on('data', function (chunk) {
+          res.write(chunk);
+        });
+      }
+
+      var etsyreq = http.request(options, callback);
+      etsyreq.end();
+      res
+    }
+    res
+  })
+
+var port = process.env.PORT || 8888;
 // http.createServer(app).listen(8080);
 http.createServer(app).listen(port);
